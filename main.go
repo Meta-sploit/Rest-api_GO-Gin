e7ca22a44819cc007442d743107bc2d21be8fca2
vasudev_parmar <vsdvprmr@gmail.com>
2023-05-26 14:48:50 +0530

    testing all function in repo are working or not

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	"newsfeeder/httpd/handler"
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"newsfeeder/repository/newsfeeds"
 )
+
+// "newsfeeder/httpd/handler"
+// "github.com/gin-gonic/gin"
 
 
 
 func main(){
-	r:=gin.Default();  
+	// r:=gin.Default();  
     
-	r.GET("/",handler.PingGet)
+	// r.GET("/",handler.PingGet)
 
-	r.Run(":8080")
+	// r.Run(":8080")
+	feed:= newsfeeds.New()
+	fmt.Print(feed)
+
+	feed.AddItem(newsfeeds.Item{"hello","world"})
+	fmt.Print(feed.GETAll())
 
 }
